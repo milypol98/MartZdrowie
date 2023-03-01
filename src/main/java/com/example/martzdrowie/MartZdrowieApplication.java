f8f50caefe08c164848a2feff2891032723ef2c0
milypol98 <milypol98@gmail.com>
2023-03-01 19:18:17 +0100

    first commit

---
+++ src/main/java/com/example/martzdrowie/MartZdrowieApplication.java
@@ -0,0 +1,13 @@
+package com.example.martzdrowie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartZdrowieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartZdrowieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/martzdrowie/MartZdrowieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.martzdrowie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartZdrowieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
